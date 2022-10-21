40a3c0b8dbaf9841c3d1de2e48f4b0336e38991f
TheHGuito1602 <hguito1602@gmail.com>
2022-10-21 13:36:46 -0500

    Hugo
    Agregue al print

--- src/PALRE/Gram.java
+++ src/PALRE/Gram.java
@@ -20,7 +20,7 @@ public class Gram {
         System.out.println("");
         System.out.println("");
         System.out.println("");
-        System.out.println("");
+        System.out.println("Hugo");
         
 }
 }
