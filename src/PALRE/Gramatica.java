1ebf0902610ce1c1f3f540fd3ce7c05f735f897c
TheHGuito1602 <hguito1602@gmail.com>
2022-10-22 17:34:13 -0500

    Ajustes 17:33
    Separación en arreglos y alternativa en tipo de dato.

--- src/PALRE/Gramatica.java
+++ src/PALRE/Gramatica.java
@@ -21,19 +21,22 @@ public class Gramatica {
     public void inicio(){
         // TODO code application logic here
         String con="";
-        String token="char x=0";
-        //int/x/=0/;
-        System.out.println("Cadena recibida: "+token);
+        String token1="String x =0 ;";
+        //int/x/=0/; <- esta es la correcta
+        System.out.println("Cadena recibida: "+token1);
         System.out.println("");
         System.out.println("<variable>-><declaracion>");
-  
-        for(int i = 0;i<token.length();i++){
-            if(token.charAt(i)!=' '){
-                con=con+token.charAt(i);
-                tipo(con);
-            }
-           
-        }
+        int dim = this.separar(token1);
+        String [] token = new String [dim];
+        token = token1.split(" ");//Separa en un arreglo las partes
+                                        //de la cadena en tokens
+        for(int i = 0; i<token.length;i++){ //
+            System.out.println(token[i]);   //Impresion para revisar las
+        }                                   //posiciones del arreglo
+        
+        
+        this.tipo(token[0]);
+        //this.id(token[1]); <- Propuesta
         
 
     }
@@ -45,7 +48,25 @@ public class Gramatica {
                 String d="double";
                 String c="char";
                 
-                
+                switch (revisar){
+                    case "int":
+                        System.out.println("<tipo>-> "+revisar+"<id><inicio_int>;");
+                        break;
+                    case "float":
+                        System.out.println("<declaracion>-> "+revisar+"<id><inicio_float>;");
+                        break;
+                    case "char":
+                        System.out.println("<declaracion>-> "+revisar+"<id><inicio_char>;");
+                        break;
+                    case "double":
+                        System.out.println("<declaracion>-> "+revisar+"<id><inicio_double>;");
+                        break;
+                    default:
+                        System.out.println("<declaracion>-> "+revisar);
+                        System.out.println("Error");
+                        break;
+                }
+                /*
                 if(i.compareTo(revisar)==0){
                     System.out.println("<tipo>-> "+revisar+"<id><inicio_int>;");
                     
@@ -61,11 +82,24 @@ public class Gramatica {
                 if(c.compareTo(revisar)==0){
                     System.out.println("<declaracion>-> "+revisar+"<id><inicio_char>;");                
 
-                }
+                }*/
+                
+                
                 System.out.println("");
             }
             
+            public int separar(String cadena){
+                int contador = 0;
+                for(int i = 0; i<cadena.length();i++){
+                    if(cadena.charAt(i)!=' '){
+                        contador++;
+                    }
+                }
+                
+                return contador;
             }
+              
+}
           /*
         if(con.compareTo(a.tipo())==0|con.compareTo("float")==0){
                     
